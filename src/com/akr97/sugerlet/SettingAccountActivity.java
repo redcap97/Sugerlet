dab92e9b2a9572b83d92550784f4f95b31587afc
Akira Midorikawa <example@sample.com>
2011-06-03 02:11:05 +0900

    Add ListView to SettingAccountActivity class.

--- src/com/akr97/sugerlet/SettingAccountActivity.java
+++ src/com/akr97/sugerlet/SettingAccountActivity.java
@@ -2,11 +2,15 @@ package com.akr97.sugerlet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class SettingAccountActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_account);
+        
+		ListView listView = (ListView)findViewById(R.id.list);
+		listView.setAdapter(new SettingAccountAdapter(this));
     }
 }
--- src/com/akr97/sugerlet/SettingAccountAdapter.java
+++ src/com/akr97/sugerlet/SettingAccountAdapter.java
@@ -1,30 +1,60 @@
 package com.akr97.sugerlet;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.BaseAdapter;
 
 public class SettingAccountAdapter extends BaseAdapter {
-	public SettingAccountAdapter(){
+	private Context context;
+	private AccountStateManager accountStateManager;
+	
+	public SettingAccountAdapter(Context context){
+		this.context = context;
+		this.accountStateManager = AccountStateManagerFactory.create(context);
 	}
 	
 	@Override
 	public int getCount() {
-		return 0;
+		return accountStateManager.getAccountSize();
 	}
 
 	@Override
 	public Object getItem(int position) {
-		return null;
+		return accountStateManager.getState(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
-		return 0;
+		return position;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		return null;
+		AccountStateManager.State state = (AccountStateManager.State)getItem(position);
+		ViewHolder holder;
+		
+		if(convertView == null){
+			LayoutInflater inflater = LayoutInflater.from(this.context);
+			convertView = inflater.inflate(R.layout.setting_account_item, null);
+			
+			holder = new ViewHolder();
+			holder.name = (TextView)convertView.findViewById(R.id.name);
+			holder.type = (TextView)convertView.findViewById(R.id.type);
+			convertView.setTag(holder);
+		}else{
+			holder = (ViewHolder)convertView.getTag();
+		}
+		holder.name.setText(state.getName());
+		holder.type.setText(state.getType());
+		
+		return convertView;
+	}
+	
+	static class ViewHolder{
+		public TextView name;
+		public TextView type;
 	}
 }
