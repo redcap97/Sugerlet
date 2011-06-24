8bdd237022d0e9cefe382a96cc3981172a1d1ea7
Akira Midorikawa <example@sample.com>
2011-06-24 17:09:08 +0900

    Add ZenkakuKatakanaToZenkakuHiragana class.

---
+++ src/com/akr97/sugerlet/util/japanese/ZenkakuKatakanaToZenkakuHiragana.java
@@ -0,0 +1,31 @@
+package com.akr97.sugerlet.util.japanese;
+
+public class ZenkakuKatakanaToZenkakuHiragana {
+	/*
+	 * ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞ
+	 * ただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽ
+	 * まみむめもゃやゅゆょよらりるれろゎわゐゑをん
+	 * 
+	 * ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾ
+	 * タダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポ
+	 * マミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ
+	 */
+	public static String zenkakuHiraganaToZenkakuKatakana(String s) {
+		StringBuffer sb = new StringBuffer(s);
+		for (int i = 0; i < sb.length(); i++) {
+			char c = sb.charAt(i);
+			if (c >= 'ァ' && c <= 'ン') {
+				sb.setCharAt(i, (char)(c - 'ァ' + 'ぁ'));
+			} else if (c == 'ヵ') {
+				sb.setCharAt(i, 'か');
+			} else if (c == 'ヶ') {
+				sb.setCharAt(i, 'け');
+			} else if (c == 'ヴ') {
+				sb.setCharAt(i, 'う');
+				sb.insert(i + 1, '゛');
+				i++;
+			}
+		}
+		return sb.toString();    
+	}
+}
