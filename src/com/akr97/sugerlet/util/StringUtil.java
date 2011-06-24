0880f8e77059898ef15c4f7264cff6263afd0957
Akira Midorikawa <example@sample.com>
2011-06-24 15:10:54 +0900

    Add StringUtil#toZenkakuHiragana method.

--- src/com/akr97/sugerlet/util/StringUtil.java
+++ src/com/akr97/sugerlet/util/StringUtil.java
@@ -4,4 +4,23 @@ public class StringUtil {
 	public static String toNonNullString(String str){
 		return str == null ? "" : str;
 	}
+
+	public static String toZenkakuHiragana(String s) {
+		StringBuffer sb = new StringBuffer(s);
+		for (int i = 0; i < sb.length(); i++) {
+			char c = sb.charAt(i);
+			if (c >= 'ァ' && c <= 'ン') {
+				sb.setCharAt(i, (char)(c - 'ァ' + 'ぁ'));
+			} else if (c == 'ヵ') {
+				sb.setCharAt(i, 'か');
+			} else if (c == 'ヶ') {
+				sb.setCharAt(i, 'け');
+			} else if (c == 'ヴ') {
+				sb.setCharAt(i, 'う');
+				sb.insert(i + 1, '゛');
+				i++;
+			}
+		}
+		return sb.toString();
+	}
 }
