989fac6e750f525e97aa587664316113b98ecda2
Akira Midorikawa <example@sample.com>
2011-06-24 17:13:36 +0900

    Fix StringUtil#toZenkakuHiragana class.

--- src/com/akr97/sugerlet/util/StringUtil.java
+++ src/com/akr97/sugerlet/util/StringUtil.java
@@ -1,26 +1,13 @@
 package com.akr97.sugerlet.util;
 
+import com.akr97.sugerlet.util.japanese.*;
+
 public class StringUtil {
 	public static String toNonNullString(String str){
 		return str == null ? "" : str;
 	}
 
 	public static String toZenkakuHiragana(String s) {
-		StringBuffer sb = new StringBuffer(s);
-		for (int i = 0; i < sb.length(); i++) {
-			char c = sb.charAt(i);
-			if (c >= 'ァ' && c <= 'ン') {
-				sb.setCharAt(i, (char)(c - 'ァ' + 'ぁ'));
-			} else if (c == 'ヵ') {
-				sb.setCharAt(i, 'か');
-			} else if (c == 'ヶ') {
-				sb.setCharAt(i, 'け');
-			} else if (c == 'ヴ') {
-				sb.setCharAt(i, 'う');
-				sb.insert(i + 1, '゛');
-				i++;
-			}
-		}
-		return sb.toString();
+		return ZenkakuKatakanaToZenkakuHiragana.zenkakuHiraganaToZenkakuKatakana(s);
 	}
 }
