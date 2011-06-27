caf949b3d86f8564c24ef016315b32b77581e7a1
Akira Midorikawa <example@sample.com>
2011-06-28 00:09:51 +0900

    Fix InitialsGroupContactsActivity class.

--- src/com/akr97/sugerlet/InitialsGroupContactsActivity.java
+++ src/com/akr97/sugerlet/InitialsGroupContactsActivity.java
@@ -6,41 +6,48 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.akr97.sugerlet.japanese.InitialsGroupSelector;
 import com.akr97.sugerlet.listitem.*;
 import com.akr97.sugerlet.model.*;
 import com.akr97.sugerlet.util.*;
 
 public class InitialsGroupContactsActivity extends ContactsActivity {
 	private Parameter params;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		this.params = new Parameter();
 		super.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
 	public String createTitle(){
 		return getString(R.string.initials_group);
 	}
-	
+
 	@Override
 	public ArrayList<ListItem> createListItems(){
-		return createListItems(getStructuredNames());
-	}
-	
-	private ArrayList<StructuredNameData> getStructuredNames(){
+		ArrayList<ListItem> items = new ArrayList<ListItem>();
+
 		StructuredNameModel model = new StructuredNameModel(this);
-		NormalizedNameList list =  NormalizedNameList.fromStructuredNames(model.getAll());
-		return list.filter(params.initialsGroup).sort().extract();
+		NormalizedNameList list = NormalizedNameList.fromStructuredNames(model.getAll())
+									.filter(params.initialsGroup).sort();
+		if(list.size() > 0){
+			items.add(new ListHeaderItem(this,
+					InitialsGroupSelector.getGroupName(params.initialsGroup)));
+			for(NormalizedName name : list){
+				items.add(new ContactListContentItem(this, name.getEntity()));
+			}
+		}
+		return items;
 	}
-	
+
 	public static Intent getIntent(Context context, char initialsGroup){
 		Intent intent = new Intent(context, InitialsGroupContactsActivity.class);
 		intent.putExtra(context.getString(R.string.key_initials_group), initialsGroup);
 		return intent;
 	}
-	
+
 	class Parameter{
 		public final Intent intent;
 		public final char initialsGroup;
