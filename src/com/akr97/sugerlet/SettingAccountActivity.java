c1343afaa441d782122c8cacbe7efe9beb7e2670
Akira Midorikawa <example@sample.com>
2011-07-06 03:50:00 +0900

    Set title of SettingAccountActivity class.

--- src/com/akr97/sugerlet/SettingAccountActivity.java
+++ src/com/akr97/sugerlet/SettingAccountActivity.java
@@ -24,6 +24,7 @@ public class SettingAccountActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setting_account);
 
+		setTitle(getString(R.string.setting_account));
 		setupSettingAccountList();
 		setupSaveButton();
 	}
